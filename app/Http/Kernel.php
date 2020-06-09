cb71dcc5f4e00e8694b956f5979412d997ed135a
Muhammed Ahmed <muhammad.farghali14@gmail.com>
2020-06-09 00:34:44 +0000

    working on OAuth Passport Package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             //            \App\Http\Middleware\TokenAuth::class,
             //            \App\Http\Middleware\BasicAuth::class,
+            'auth:api'
         ],
     ];
 
